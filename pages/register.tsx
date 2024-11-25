3d80614b5e28f829024719cc0eb04adc4f3fc1f9
stanondieki <ondiekistanley21@gmail.com>
2024-11-25 13:40:34 +0300

    register commit

---
+++ pages/register.tsx
@@ -0,0 +1,10 @@
+import Registration from "@/components/registration";
+
+const RegisterPage = () => {
+    return(
+        <div>
+         <Registration/>
+        </div>
+    )
+};
+export default RegisterPage;
